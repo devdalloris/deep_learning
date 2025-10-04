9f404f0f2dfa48a634c78f95e81ed1550f2c6c77
Dalloris <dollydolly2021dolly@gmail.com>
2025-10-04 10:09:01 +0500

    lesson 3 homework added

--- unit_test/app.py
+++
@@ -1,8 +0,0 @@
-def calculate_area(width, height):
-    if width < 0 or height <0:
-        raise ValueError("Ширина и высота должны быть неотрицательными.")
-    return width * height
-
-def is_palindrome(text):
-    clean_text = ''.join(filter(str.isalnum, text)).lower()
-    return clean_text == clean_text[::-1]
